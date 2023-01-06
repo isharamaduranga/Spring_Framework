cf1986227f5b258853255ef9d08095ab592842e4
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-06 19:08:51 +0530

    Implemnted  life-Cycle Methods from A  & B  Classes.

--- SpringFramework/04_DI-Spring/src/lk/ijse/spring/pojo/A.java
+++ SpringFramework/04_DI-Spring/src/lk/ijse/spring/pojo/A.java
@@ -8,18 +8,53 @@
 
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class A {
+public class A implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
-    private B b;
+    @Autowired
+    private B b ;
 
-    public A() {
+    public A()  {
         System.out.println("Instantiated : A");
     }
 
     public void sendRequestB(){
         b.callMe();
     }
+
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean A :Bean Name Aware");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean A :Bean Factory Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Bean A :Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean A : Initializing Bean(Ready Bean)");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean A : Disposable Bean(destroy Bean)");
+    }
+
+
+
 }
--- SpringFramework/04_DI-Spring/src/lk/ijse/spring/pojo/B.java
+++ SpringFramework/04_DI-Spring/src/lk/ijse/spring/pojo/B.java
@@ -8,14 +8,43 @@
 
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class B {
+public class B implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public B() {
         System.out.println("Instantiated : B");
     }
     public void callMe(){
         System.out.println("Received a Call A");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean B :Bean Name Aware");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean B :Bean Factory Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Bean B :Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean B : Initializing Bean(Ready Bean)");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean B : Disposable Bean(destroy Bean)");
     }
 }
