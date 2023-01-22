dc4e107cbe2de152a07d10d2c315cbd18534ae8e
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-22 16:30:33 +0530

    Content  Updated with Maven Project 13-SpringWithMaven..

--- SpringFramework/13_SpringWithMaven/src/main/java/lk/ijse/spring/controllers/CustomerController.java
+++ SpringFramework/13_SpringWithMaven/src/main/java/lk/ijse/spring/controllers/CustomerController.java
@@ -8,10 +8,22 @@
 
 package lk.ijse.spring.controllers;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin
 public class CustomerController {
+
+    @PostMapping
+    public String  saveCustomer(CustomerDTO dto){
+        System.out.println(dto.toString());
+
+        return "Customer Added";
+    }
+
 }
