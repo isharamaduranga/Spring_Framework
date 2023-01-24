2ab3e265e4e6f81332c449683721fa8e6992f434
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-24 13:49:28 +0530

    Added item Entity..

---
+++ SpringFramework/15_Spring_Data_JPA/src/main/java/lk/ijse/spring/entity/Item.java
@@ -0,0 +1,73 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: SpringFramework
+ * Date        : 1/24/2023
+ * Time        : 1:47 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    private String code;
+    private String description;
+    private int qtyOnHand;
+    private double unitPrice;
+
+    public Item() {
+    }
+
+    public Item(String code, String description, int qtyOnHand, double unitPrice) {
+        this.code = code;
+        this.description = description;
+        this.qtyOnHand = qtyOnHand;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                ", qtyOnHand=" + qtyOnHand +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
